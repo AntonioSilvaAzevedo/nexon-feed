dfc8444f57793195ac06bf26864805fc32ce3561
Antonio Carlos <antonie.dev@gmail.com>
2022-07-15 19:11:18 -0300

    Siderbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+import styles from './App.module.css'
+import './global.css'
+
 import { Header } from "./components/Header"
-import {Post} from "./Post"
+import {Post } from './Post';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
  return (
   <div>
     <Header />
-    <Post
-      author='Antonio Carlos'
-      content='josejosejosejosejose'
-    /> 
-    <Post
-      author=' Carlos'
-      content='peido peido peido'
-    />  
+
+    <div className={styles.wrapper}>
+      <aside>
+        <Sidebar />
+      </aside>
+      <main>
+        <Post 
+          author='Antonio carlos'
+          content='jose jose jose jose jose jose jose jose jose jose jose'
+        />
+        <Post 
+        author='Antonio carlos'
+        content='jose jose jose jose jose jose jose jose jose jose jose'
+        />
+      </main>
+    </div>
+      
   </div>
   
  )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+
+import igniteLogo from '../assets/ignite-logo.svg';
+
+console.log(igniteLogo);
+
 
 export function Header() {
   return (
     <header className={styles.header}>
       <strong>nexon-feed</strong>
+      <img src={igniteLogo} alt="Logo ignite" />
     </header>
   )
 }
